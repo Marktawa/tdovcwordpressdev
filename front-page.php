69a4470a946e9b9bdd4bae607d73ab984ca0e76d
Mark <markmunyaka@gmail.com>
2022-02-18 15:13:42 +0200

    Added font awesome

---
+++ front-page.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="/wp-content/themes/tdovcwordpresstheme/style.css">
+  
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+  <!--<script src="https://kit.fontawesome.com/9ac95a91c7.js" crossorigin="anonymous"></script>-->
+  <link rel="stylesheet" href="assets/static/fontawesome-5153/css/all.css">
+  <script src="scripts/header.js"></script>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twatasha Disabled and OVC Organization</title>
+</head>
+
+<body>
+  <div id="page-wrapper">
+    <header id="header-large">
+      <a href="#" id="logo-placeholder">Twatasha Disabled & OVC Organisation</a>
+      <nav id="navbar">
+        <a href="pages/about-us.html">About Us</a>
+        <a href="pages/news/index.html">News</a>
+        <a href="pages/our-work.html">Our Work</a>
+        <a href="pages/contact-us.html">Contact Us</a>
+        <a href="pages/donate.html">Donate</a>
+      </nav>
+    </header>
+    <header id="header-small">
+      <a href="#" id="logo-small">Twatasha Disabled & OVC Organisation</a>
+      <a href="#" id="logo-smallest">TDOVC</a>
+      <nav id="navbar-small">
+        <a href="pages/about-us.html">About Us</a>
+        <a href="pages/news/index.html">News</a>
+        <a href="pages/our-work.html">Our Work</a>
+        <a href="pages/contact-us.html">Contact Us</a>
+        <a href="pages/donate.html">Donate</a>
+      </nav>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </header>
+
+    <main id="main">
+      <section id="hero">
+        <h1 id="welcome-text">Twatasha Disabled and OVC Organisation <br>welcomes you!</h1>
+        <p>To reduce Disabilities through Rehabilitation<br> and to provide Psychosocial <br> Needs to our communities
+        </p>
+      </section>
+      <section id="intro-video">
+        <div id="video-container">
+        <iframe src="https://www.youtube.com/embed/jGJXhuoVpic" title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          loading="lazy"
+          allowfullscreen></iframe>
+          </div>
+      </section>
+      <section id="summary">
+        <div class="summary" id="volunteer">
+          <div class="level">
+            <h2>Volunteer</h2>
+          </div>
+          <div class="image-container-summary">
+            <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-volunteer.jpg"
+              loading="lazy" alt="Twatasha Disabled and OVC Organization">
+          </div>
+          <p class="summary-text">
+            We welcome all helpers to our organisation. This includes experts as well as caregivers.
+          </p>
+          <a href="pages/jobs.html"><button class="btn">Join us</button></a>
+        </div>
+        <div class="summary" id="work">
+          <div class="level">
+            <h2>Our Work</h2>
+          </div>
+          <div class="image-container-summary">
+            <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/hero-image-0.jpg" loading="lazy"
+              alt="Twatasha Disabled and OVC Organization">
+          </div>
+          <p class="summary-text">
+            Our work is helping disabled people to live, learn and earn as independently as they choose across the
+            globe.
+          </p>
+          <a href="pages/our-work.html"><button class="btn">Learn more</button></a>
+        </div>
+        <div class="summary" id="donate">
+          <div class="level">
+            <h2>Donate</h2>
+          </div>
+          <div class="image-container-summary">
+            <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-donate.jpg"
+              loading="lazy" alt="Twatasha Disabled and OVC Organization">
+          </div>
+          <p class="summary-text">
+            Support our mission so more disabled people can live, learn and earn. No matter the amount
+          </p>
+          <a href="pages/donate.html"><button class="btn">Donate</button></a>
+        </div>
+      </section>
+      <section id="news-events">
+        <h2 class="news-header">News happening at Twatasha Disabled and OVC organisation</h2>
+        <div class="row">
+          <div class="column" id="announcements">
+            <h2 class="news-event-title">Announcements</h2>
+            <div class="image-container-summary">
+              <img
+                src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-news-disability-cert.jpg"
+                loading="lazy" alt="Twatasha Disabled and OVC Organization">
+            </div>
+            <h3 class="news-event-heading">Disability Certification and issuance of Disability Cards</h3>
+            <h6 class="news-event-date">7 June 2021</h6>
+            <p class="news-event-text">There will be disability certification and issuance of disability cards on
+              Tuesday the 7th of June 2021 at Twatasha Community School. You are all invited not to miss this rare
+              opportunity as Zambia Agency for Persons with Disabilities will be in Kafue.</p>
+          </div>
+          <div class="column">
+            <h2 class="news-event-title">Stories</h2>
+            <div class="image-container-summary">
+              <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/2021%20FSP%20ALLOCATION%20FOR%20PERSONS%20WITH%20DISABILITIES%20MEETING-image-2.jpg"
+                loading="lazy" alt="People sitting outside of Twatasha Disabled and OVC Organization">
+            </div>
+            <h3 class="news-event-heading">2021 FSP Allocation for persons with disabilities meeting</h3>
+            <h6 class="news-event-date">11 November 2021</h6>
+            <p class="news-event-text"> On Thursday the 11th of November 2021, we met as the Disability People’s Organisations Consortium (DPOC) for Kafue District, Lusaka Province, Zambia together with the representative for the Ministry of Community Development and Social Welfare, Leah Mumbi...</p>
+          </div>
+          <div class="column">
+            <h2 class="news-event-title">Events</h2>
+            <div class="image-container-summary">
+              <img
+                src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-kafue-residents.jpg"
+                loading="lazy" alt="Twatasha Disabled and OVC Organization">
+            </div>
+            <h3 class="news-event-heading">Kafue Residents Community Meeting</h3>
+            <h6 class="news-event-date">23 May 2021</h6>
+            <p class="news-event-text">On 23rd May 2021, we had a community meeting with Kafue residents. We discussed
+              how to support persons with disabilities and their families. The community wanted to know how the
+              government and other stakeholders are helping our communities through social cash transfer.</p>
+          </div>
+        </div>
+        <a href="pages/news/"> <button id="news-events-btn">See all our latest news</button></a>
+      </section>
+      <section id="partners">
+        <h2 class="news-header">Our Partners</h2>
+        <div class="partner-group">
+          <a href="http://www.cheshirehomessocietyzambia.org/" target="_blank">
+            <div class="partner-block">
+              <img
+                src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-leonard-cheshire-logo.png"
+                loading="lazy" alt="Leonard Cheshire Disability">
+              <h5>Leonard<br> Cheshire<br> Disability</h5>
+            </div>
+          </a>
+          <a href="https://www.lilianefonds.org/" target="_blank">
+            <div class="partner-block">
+              <img
+                src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-lilliane-fonds-logo.png"
+                loading="lazy" alt="Liliane Fonds">
+              <h5>Liliane<br> Fonds</h5>
+            </div>
+          </a>
+          <a href="https://www.zafod.net" target="_blank">
+            <div class="partner-block">
+              <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-zafod-logo.jpg"
+                loading="lazy" alt="ZAFOD">
+              <h5>ZAFOD</h5>
+            </div>
+          </a>
+          <a href="https://afri-can.org" target="_blank">
+            <div class="partner-block">
+              <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-cbr-logo.jpg"
+                loading="lazy" alt="CBR">
+              <h5>CBR</h5>
+            </div>
+          </a>
+          <a href="http://www.zapd.org.zm" target="_blank">
+            <div class="partner-block">
+              <img src="https://raw.githubusercontent.com/Marktawa/twatasha/main/images/twatasha-home-zapd-logo.png"
+                loading="lazy" alt="ZAPD">
+              <h5>ZAPD</h5>
+            </div>
+          </a>
+        </div>
+      </section>
+    </main>
+    <footer id="footer">
+      <div class="container-outer">
+        <div class="site-footer-container-inner">
+          <div class="site-footer-navigation-primary">
+            <div class="site-footer-navigation-primary-inner">
+              <h2 class="site-footer-title">Quick links</h2>
+              <div class="region-region-footer-first">
+                <nav id="block-footer" class="block block-menu navigation menu--footer" role="navigation">
+                  <ul class="menu">
+                    <li class="menu-item">
+                      <a href="pages/our-work.html">Our Work</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="pages/contact-us.html">Contact us</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="https://www.facebook.com/tdovc2012/photos" target="_blank">Photo Gallery</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="pages/jobs.html">Jobs</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="pages/projects.html">Projects</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div class="site-footer-cta">
+            <div class="site-footer-cta-inner">
+              <h2 class="site-footer-title">Stay up to date</h2>
+              <div class="region-region-footer-second">
+                <div id="block-footersecondarycontent-2" class="block-footersecondarycontent-2">
+                  <div class="text-formatted-field">
+                    <p>Sign up to our newsletter to be kept up to date with our latest news and events.</p>
+                  </div>
+                </div>
+                <div class="signup-form block" id="block-signupblock">
+                  <form id="form" action="#">
+                    <input name="email" id="email" type="email" placeholder="Enter your email address" required />
+                    <input id="submit" type="submit" value="Sign Up" class="btn" />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="site-footer-social">
+            <div class="site-footer-social-inner">
+              <h2 class="site-footer-title">Connect with us</h2>
+              <div class="region-region-footer-third">
+                <div id="block-socialmedialinks" class="block-social-media-links">
+                  <ul class="social-media-links-platforms">
+                    <li>
+                      <a href="https://www.facebook.com/tdovc2012" target="_blank">
+                        <span class='fab fa-facebook fa-lg' aria-hidden='true'></span>
+                        <span class="visually-hidden">Follow us on Facebook</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="mailto:tdovc2012@gmail.com" target="_blank">
+                        <span class='fas fa-envelope fa-lg' aria-hidden='true'></span>
+                        <span class="visually-hidden">E-mail us</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://wa.me/260972551791" target="_blank">
+                        <span class='fab fa-whatsapp fa-lg' aria-hidden='true'></span>
+                        <span class="visually-hidden">Follow us on Whatsapp</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="site-footer-container-inner" id="second-footer-section">
+          <div class="site-footer-navigation-secondary">
+            <h2 class="visually-hidden">Additional Twatasha Disabled and OVC Organisation information</h2>
+            <div class="region-region-footer-fourth">
+              <nav id="block-footersecondary" class="block block-menu navigation menu--footer-secondary"
+                role="navigation">
+                <ul id="mini-footer-menu" class="menu">
+                  <li class="menu-item">
+                    <a href="pages/privacy-policy.html">Privacy Notice</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="pages/accessibility.html">Accessibility</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="pages/terms-and-conditions.html">Terms and Conditions</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div class="site-footer-col-container">
+            <div class="site-footer-col">
+              <div class="site-footer-additional">
+                <div class="region region-footer-fifth">
+                  <div id="block-footer-fifth-content" class="block-footerfifth-content">
+                    <div class="clearfix-text-formatted-field">
+                      <p>Twatasha Disabled and OVC Organisation is a company limited by guarantee, registered in
+                        Zambia, and a registered non-governmental organisation (Zambia). Registered office: Kafue,
+                        Lusaka Province, Zambia.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
