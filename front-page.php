68e0c2038ebad1de90a9313d1d9d062b878babec
Mark <markmunyaka@gmail.com>
2022-02-18 17:25:46 +0200

    Added link to header.js in head section

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
   <link rel="stylesheet" href="/wp-content/themes/tdovcwordpresstheme/assets/icons/fontawesome-5153/css/all.css">
-  <script src="scripts/header.js"></script>
+  <script src="/wp-content/themes/tdovcwordpresstheme/assets/js/header.js"></script>
 
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
