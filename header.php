3d27fddf9d42d77c5758336d68b895cab6417979
McTawa <markmunyaka@gmail.com>
2022-03-07 13:17:45 +0000

    Set up front page to display 3 latest posts

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="/wp-content/themes/tdovcwordpressdev/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="/wp-content/themes/tdovcwordpressdev/assets/icons/fontawesome-5153/css/all.css">
+  <script src="/wp-content/themes/tdovcwordpressdev/assets/js/header.js"></script>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php the_title(); ?></title>
+</head>
+
+<body>
+  <div id="page-wrapper">
+    <header id="header-large">
+      <a href="#" id="logo-placeholder">Twatasha Disabled & OVC Organisation</a>
+      <nav id="navbar">
+        <a href="pages/about-us.html">About Us</a>
+        <a href="pages/news/index.html">News</a>
+        <a href="pages/our-work.html">Our Work</a>
+        <a href="pages/contact-us.html">Contact Us</a>
+        <a href="pages/donate.html">Donate</a>
+      </nav>
+    </header>
+    <header id="header-small">
+      <a href="#" id="logo-small">Twatasha Disabled & OVC Organisation</a>
+      <a href="#" id="logo-smallest">TDOVC</a>
+      <nav id="navbar-small">
+        <a href="pages/about-us.html">About Us</a>
+        <a href="pages/news/index.html">News</a>
+        <a href="pages/our-work.html">Our Work</a>
+        <a href="pages/contact-us.html">Contact Us</a>
+        <a href="pages/donate.html">Donate</a>
+      </nav>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </header>
